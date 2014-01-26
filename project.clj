b60d15298f783a4918ea2e5e5c010327fe0978ae
feldi <peter.feldtmann@gmail.com>
2014-01-26 14:21:33 +0100

    bump clojure version

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :min-lein-version "2.0.0"
   :profiles {:dev
              {:dependencies [[com.stuartsierra/lazytest "1.1.2"]]}}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :repositories {"stuartsierra-releases"
                  "http://stuartsierra.com/maven2"}
   :description "Internationalization for Clojure")
